f3a25c5cf81efacacdcde3bd426819141f6a21fc
Alex Romano <alex.p.romano@gmail.com>
2018-02-27 15:02:11 -0700

    Initial commit

---
+++ Source/Fortnite/FortniteGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FortniteGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORTNITE_API AFortniteGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
